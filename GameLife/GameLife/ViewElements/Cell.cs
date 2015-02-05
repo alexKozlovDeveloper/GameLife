6b2bc4a23fd43b74d641313535ae9acc59bbe1e6
Aliaksei Kazlou <aliaksei_kazlou@epam.com>
2015-02-05 14:25:35 +0300

    update, add classes Cell and GridCell

---
+++ GameLife/GameLife/ViewElements/Cell.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace GameLife.ViewElements
+{
+    class Cell
+    {
+        private int _width;
+        private int _height;
+
+        private Grid _grid;
+        Rectangle _rectangle;
+
+        public Cell(int h, int w, int x, int y, Grid grid)
+        {
+            _width = w;
+            _height = h;
+
+            _grid = grid;
+
+            _rectangle = new Rectangle();
+
+            _rectangle.Width = _width;
+            _rectangle.Height = _height;
+
+            //_rectangle.Margin
+
+            //_grid.Cohildren.Add(_rectangle);
+
+            //_rectangle.Fill = new SolidColorBrush(System.Windows.Media.Colors.Green); 
+        }
+
+        public void SetColor(Color color)
+        { 
+            _rectangle.Fill = new SolidColorBrush(color); 
+        }
+    }
+}
---
+++ GameLife/GameLife/ViewElements/CellsGrid.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GameLife.ViewElements
+{
+    class CellsGrid
+    {
+        private List<List<Cell>> _cells;
+        private Grid _grid;
+
+        private int _widthCount;
+        private int _heightCount;
+
+        private int cellWidth = 10;
+        private int cellHeight = 10;
+
+        public CellsGrid(int wCount, int hCount, Grid grid)
+        {
+            _grid = grid;
+
+            _widthCount = wCount;
+            _heightCount = hCount;
+
+            _cells = new List<List<Cell>>();
+
+            for (int i = 0; i < _widthCount; i++)
+            {
+                var list = new List<Cell>();
+
+                for (int j = 0; j < _heightCount; j++)
+                {
+                    list.Add(new Cell(cellWidth, cellHeight, i, j, _grid));
+                }
+
+                _cells.Add(list);
+            }
+
+            _grid.Width = GridWidth;
+            _grid.Height = GridHeight;
+        }
+
+        public int GridWidth
+        {
+            get { return _widthCount * cellWidth; } 
+        }
+
+        public int GridHeight
+        {
+            get { return _heightCount * cellHeight; } 
+        }
+    }
+}
